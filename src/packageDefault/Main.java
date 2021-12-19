3cbb0d90573426b7e86ce6bab6ad7a3f994efc93
luigigalbusera <galbu.luigi@gmail.com>
2021-12-19 17:02:46 +0100

    cambiato con sei una capra

--- src/packageDefault/Main.java
+++ src/packageDefault/Main.java
@@ -3,7 +3,7 @@ package packageDefault;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println ("Halloooo ELAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaIA ");
+        System.out.println ("Sei una capra ");
 	// write your code here
     }
 }
